66526527eebff7dc1fa3b76d93f7fafb5ab1fd03
Iryna Bakhur <ira.bakhur@icloud.com>
2022-06-10 13:03:09 +0300

    refactor: change getting user name info on app start

--- src/general/getUserName.js
+++ src/general/getUserName.js
@@ -1,13 +1,15 @@
 export const getUserName = (args) => {
-  const defaultUserName = 'unkhown user';
+  const defaultUserName = 'unkhown_user';
   let userName;
 
   if (args.length) {
     const prefix = '--username=';
-    const prefixIndex = args.findIndex((arg) => arg.startsWith(prefix));
-    userName = prefixIndex !== -1
-      ? args[prefixIndex].slice(prefix.length).concat(' ', args.slice(prefixIndex + 1).join(' '))
-      : undefined;
+    const userNameArg = args.find((arg) => arg.startsWith(prefix));
+    userName = userNameArg ? userNameArg.slice(prefix.length) : undefined;
+    // const prefixIndex = args.findIndex((arg) => arg.startsWith(prefix));
+    // userName = prefixIndex !== -1
+    //   ? args[prefixIndex].slice(prefix.length).concat(' ', args.slice(prefixIndex + 1).join(' '))
+    //   : undefined;
   }
 
   return userName || defaultUserName;
